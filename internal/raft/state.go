8af00489e937c44b5e5349c029262b61f3c3c9de
sanchar127 <msan53366@gmail.com>
2026-09-08 19:38:25 +0545

    feat:definning state

--- internal/raft/state.go
+++ internal/raft/state.go
@@ -1,1 +1,24 @@
 package raft
+
+type PersistentState struct {
+	CurrentTerm Term
+	VotedFor    NodeID
+}
+
+type VolatileState struct {
+	CommitIndex LogIndex
+	LastApplied LogIndex
+}
+
+type LeaderState struct {
+	NextIndex  map[NodeID]LogIndex
+	MatchIndex map[NodeID]LogIndex
+}
+
+type State struct {
+	Persistent PersistentState
+	Volatile   VolatileState
+	Leader     LeaderState
+	Role       Role
+	LeaderID   NodeID
+}
