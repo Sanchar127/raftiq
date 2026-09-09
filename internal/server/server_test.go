0c2fbe91cd6eb3209bcdb4e631f2b1f311d9f874
sanchar127 <msan53366@gmail.com>
2026-09-09 22:51:41 +0545

    feat:implement delete Linearizable

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -132,3 +132,42 @@ func TestServerPutAndGet(t *testing.T) {
 		t.Fatalf("expected value %q, got %q", "raftiq", string(value))
 	}
 }
+
+func TestServerDeleteAndGet(t *testing.T) {
+	nodeA := raft.NewRaftNode("A")
+	nodeB := raft.NewRaftNode("B")
+	nodeC := raft.NewRaftNode("C")
+
+	nodeA.SetPeers([]raft.Peer{nodeB, nodeC})
+
+	store := kv.NewStore()
+	server := NewServer(nodeA, store)
+
+	nodeA.Start()
+	defer nodeA.Stop()
+
+	server.Start()
+	defer server.Stop()
+
+	waitForLeader(t, nodeA)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if err := server.Put(ctx, "name", []byte("raftiq")); err != nil {
+		t.Fatalf("Put() returned error: %v", err)
+	}
+
+	if err := server.Delete(ctx, "name"); err != nil {
+		t.Fatalf("Delete() returned error: %v", err)
+	}
+
+	value, ok, err := server.Get(ctx, "name")
+	if err != nil {
+		t.Fatalf("Get() returned error: %v", err)
+	}
+
+	if ok {
+		t.Fatalf("expected key to be deleted, got value %q", value)
+	}
+}
