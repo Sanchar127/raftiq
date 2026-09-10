125b3946ba1b6501f38b335448ecff4f4501e60b
sanchar127 <msan53366@gmail.com>
2026-09-10 23:20:13 +0545

    feat:raft service test

---
+++ internal/transport/raft_service_test.go
@@ -0,0 +1,263 @@
+package transport
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	raftiqv1 "github.com/sanchar127/raftiq/api/proto"
+	"github.com/sanchar127/raftiq/internal/raft"
+	"github.com/stretchr/testify/require"
+)
+
+type fakeRaftNode struct {
+	requestVoteArgs      raft.RequestVoteArgs
+	appendEntriesArgs    raft.AppendEntriesArgs
+	installSnapshotArgs  raft.InstallSnapshotArgs
+	requestVoteReply     raft.RequestVoteReply
+	appendEntriesReply   raft.AppendEntriesReply
+	installSnapshotReply raft.InstallSnapshotReply
+}
+
+func (f *fakeRaftNode) RequestVote(args raft.RequestVoteArgs) raft.RequestVoteReply {
+	f.requestVoteArgs = args
+	return f.requestVoteReply
+}
+
+func (f *fakeRaftNode) AppendEntries(args raft.AppendEntriesArgs) raft.AppendEntriesReply {
+	f.appendEntriesArgs = args
+	return f.appendEntriesReply
+}
+
+func (f *fakeRaftNode) InstallSnapshot(args raft.InstallSnapshotArgs) raft.InstallSnapshotReply {
+	f.installSnapshotArgs = args
+	return f.installSnapshotReply
+}
+
+func TestNewRaftService(t *testing.T) {
+	t.Parallel()
+
+	service, err := NewRaftService(&fakeRaftNode{})
+
+	require.NoError(t, err)
+	require.NotNil(t, service)
+}
+
+func TestNewRaftServiceRejectsNilNode(t *testing.T) {
+	t.Parallel()
+
+	service, err := NewRaftService(nil)
+
+	require.Error(t, err)
+	require.Nil(t, service)
+	require.ErrorContains(t, err, "raft node is required")
+}
+
+func TestRaftServiceRequestVote(t *testing.T) {
+	t.Parallel()
+
+	node := &fakeRaftNode{
+		requestVoteReply: raft.RequestVoteReply{
+			Term:        7,
+			VoterID:     "node-2",
+			VoteGranted: true,
+		},
+	}
+
+	service, err := NewRaftService(node)
+	require.NoError(t, err)
+
+	response, err := service.RequestVote(context.Background(), &raftiqv1.RequestVoteRequest{
+		Term:         5,
+		CandidateId:  "node-1",
+		LastLogIndex: 42,
+		LastLogTerm:  4,
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, uint64(7), response.GetTerm())
+	require.Equal(t, "node-2", response.GetVoterId())
+	require.True(t, response.GetVoteGranted())
+
+	require.Equal(t, raft.RequestVoteArgs{
+		Term:         5,
+		CandidateID:  "node-1",
+		LastLogIndex: 42,
+		LastLogTerm:  4,
+	}, node.requestVoteArgs)
+}
+
+func TestRaftServiceRequestVoteRejectsNilRequest(t *testing.T) {
+	t.Parallel()
+
+	service, err := NewRaftService(&fakeRaftNode{})
+	require.NoError(t, err)
+
+	response, err := service.RequestVote(context.Background(), nil)
+
+	require.Error(t, err)
+	require.Nil(t, response)
+	require.ErrorContains(t, err, "request vote request is required")
+}
+
+func TestRaftServiceAppendEntries(t *testing.T) {
+	t.Parallel()
+
+	node := &fakeRaftNode{
+		appendEntriesReply: raft.AppendEntriesReply{
+			Term:       9,
+			FollowerID: "node-2",
+			Success:    true,
+		},
+	}
+
+	service, err := NewRaftService(node)
+	require.NoError(t, err)
+
+	response, err := service.AppendEntries(context.Background(), &raftiqv1.AppendEntriesRequest{
+		Term:         8,
+		LeaderId:     "node-1",
+		PrevLogIndex: 10,
+		PrevLogTerm:  7,
+		Entries: []*raftiqv1.LogEntry{
+			{
+				Index: 11,
+				Term:  8,
+				Data:  []byte("first"),
+			},
+			{
+				Index: 12,
+				Term:  8,
+				Data:  []byte("second"),
+			},
+		},
+		LeaderCommit: 10,
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, uint64(9), response.GetTerm())
+	require.Equal(t, "node-2", response.GetFollowerId())
+	require.True(t, response.GetSuccess())
+
+	require.Equal(t, raft.AppendEntriesArgs{
+		Term:         8,
+		LeaderID:     "node-1",
+		PrevLogIndex: 10,
+		PrevLogTerm:  7,
+		Entries: []raft.LogEntry{
+			{
+				Index: 11,
+				Term:  8,
+				Data:  []byte("first"),
+			},
+			{
+				Index: 12,
+				Term:  8,
+				Data:  []byte("second"),
+			},
+		},
+		LeaderCommit: 10,
+	}, node.appendEntriesArgs)
+}
+
+func TestRaftServiceAppendEntriesRejectsNilEntry(t *testing.T) {
+	t.Parallel()
+
+	service, err := NewRaftService(&fakeRaftNode{})
+	require.NoError(t, err)
+
+	response, err := service.AppendEntries(context.Background(), &raftiqv1.AppendEntriesRequest{
+		Entries: []*raftiqv1.LogEntry{
+			nil,
+		},
+	})
+
+	require.Error(t, err)
+	require.Nil(t, response)
+	require.ErrorContains(t, err, "nil log entry")
+}
+
+func TestRaftServiceAppendEntriesRejectsNilRequest(t *testing.T) {
+	t.Parallel()
+
+	service, err := NewRaftService(&fakeRaftNode{})
+	require.NoError(t, err)
+
+	response, err := service.AppendEntries(context.Background(), nil)
+
+	require.Error(t, err)
+	require.Nil(t, response)
+	require.ErrorContains(t, err, "append entries request is required")
+}
+
+func TestRaftServiceInstallSnapshot(t *testing.T) {
+	t.Parallel()
+
+	node := &fakeRaftNode{
+		installSnapshotReply: raft.InstallSnapshotReply{
+			Term:       12,
+			FollowerID: "node-3",
+			Success:    true,
+		},
+	}
+
+	service, err := NewRaftService(node)
+	require.NoError(t, err)
+
+	response, err := service.InstallSnapshot(context.Background(), &raftiqv1.InstallSnapshotRequest{
+		Term:              11,
+		LeaderId:          "node-1",
+		LastIncludedIndex: 100,
+		LastIncludedTerm:  10,
+		Data:              []byte("snapshot-data"),
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, uint64(12), response.GetTerm())
+	require.Equal(t, "node-3", response.GetFollowerId())
+	require.True(t, response.GetSuccess())
+
+	require.Equal(t, raft.InstallSnapshotArgs{
+		Term:              11,
+		LeaderID:          "node-1",
+		LastIncludedIndex: 100,
+		LastIncludedTerm:  10,
+		Data:              []byte("snapshot-data"),
+	}, node.installSnapshotArgs)
+}
+
+func TestRaftServiceInstallSnapshotRejectsNilRequest(t *testing.T) {
+	t.Parallel()
+
+	service, err := NewRaftService(&fakeRaftNode{})
+	require.NoError(t, err)
+
+	response, err := service.InstallSnapshot(context.Background(), nil)
+
+	require.Error(t, err)
+	require.Nil(t, response)
+	require.ErrorContains(t, err, "install snapshot request is required")
+}
+
+func TestRaftServicePreservesRequestDataIndependently(t *testing.T) {
+	t.Parallel()
+
+	node := &fakeRaftNode{}
+	service, err := NewRaftService(node)
+	require.NoError(t, err)
+
+	data := []byte("original")
+
+	_, err = service.InstallSnapshot(context.Background(), &raftiqv1.InstallSnapshotRequest{
+		Data: data,
+	})
+	require.NoError(t, err)
+
+	data[0] = 'X'
+
+	require.Equal(t, []byte("original"), node.installSnapshotArgs.Data)
+}
+
+var _ raftRPC = (*fakeRaftNode)(nil)
+
+var _ = errors.New
