85113e338b9358f2d0b7a99369d0981e0a3217c0
sanchar127 <msan53366@gmail.com>
2026-09-15 00:38:38 +0545

    feat:implementing decode leave joint configuration

--- internal/raft/config_entry.go
+++ internal/raft/config_entry.go
@@ -391,3 +391,240 @@ func encodeConfigurationPayload(
 
 	return nil
 }
+
+func DecodeEnterJointConfigurationEntry(
+	data []byte,
+) (model.Configuration, model.Configuration, error) {
+	if len(data) > maxConfigurationPayload {
+		return model.Configuration{}, model.Configuration{}, errors.New(
+			"joint configuration entry exceeds maximum payload size",
+		)
+	}
+
+	reader := bytes.NewReader(data)
+
+	if err := validateConfigurationEntryEnvelope(
+		reader,
+		configurationEntryTypeEnterJoint,
+	); err != nil {
+		return model.Configuration{}, model.Configuration{}, err
+	}
+
+	oldConfiguration, err := decodeConfigurationPayload(reader)
+	if err != nil {
+		return model.Configuration{}, model.Configuration{}, fmt.Errorf(
+			"decode old configuration: %w",
+			err,
+		)
+	}
+
+	newConfiguration, err := decodeConfigurationPayload(reader)
+	if err != nil {
+		return model.Configuration{}, model.Configuration{}, fmt.Errorf(
+			"decode new configuration: %w",
+			err,
+		)
+	}
+
+	if reader.Len() != 0 {
+		return model.Configuration{}, model.Configuration{}, errors.New(
+			"joint configuration entry contains trailing data",
+		)
+	}
+
+	return oldConfiguration, newConfiguration, nil
+}
+
+func DecodeLeaveJointConfigurationEntry(
+	data []byte,
+) (model.Configuration, error) {
+	if len(data) > maxConfigurationPayload {
+		return model.Configuration{}, errors.New(
+			"leave-joint configuration entry exceeds maximum payload size",
+		)
+	}
+
+	reader := bytes.NewReader(data)
+
+	if err := validateConfigurationEntryEnvelope(
+		reader,
+		configurationEntryTypeLeaveJoint,
+	); err != nil {
+		return model.Configuration{}, err
+	}
+
+	configuration, err := decodeConfigurationPayload(reader)
+	if err != nil {
+		return model.Configuration{}, fmt.Errorf(
+			"decode new configuration: %w",
+			err,
+		)
+	}
+
+	if reader.Len() != 0 {
+		return model.Configuration{}, errors.New(
+			"leave-joint configuration entry contains trailing data",
+		)
+	}
+
+	return configuration, nil
+}
+
+func validateConfigurationEntryEnvelope(
+	reader *bytes.Reader,
+	expectedType byte,
+) error {
+	var magic uint32
+
+	if err := binary.Read(
+		reader,
+		binary.BigEndian,
+		&magic,
+	); err != nil {
+		return fmt.Errorf(
+			"decode configuration magic: %w",
+			err,
+		)
+	}
+
+	if magic != configurationEntryMagic {
+		return errors.New(
+			"invalid configuration entry magic",
+		)
+	}
+
+	version, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf(
+			"decode configuration version: %w",
+			err,
+		)
+	}
+
+	if version != configurationEntryVersion {
+		return fmt.Errorf(
+			"unsupported configuration entry version: %d",
+			version,
+		)
+	}
+
+	entryType, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf(
+			"decode configuration entry type: %w",
+			err,
+		)
+	}
+
+	if entryType != expectedType {
+		return fmt.Errorf(
+			"unexpected configuration entry type: %d",
+			entryType,
+		)
+	}
+
+	return nil
+}
+
+func decodeConfigurationPayload(
+	reader *bytes.Reader,
+) (model.Configuration, error) {
+	var voterCount uint32
+
+	if err := binary.Read(
+		reader,
+		binary.BigEndian,
+		&voterCount,
+	); err != nil {
+		return model.Configuration{}, fmt.Errorf(
+			"decode voter count: %w",
+			err,
+		)
+	}
+
+	if voterCount == 0 {
+		return model.Configuration{}, errors.New(
+			"configuration must contain at least one voter",
+		)
+	}
+
+	if voterCount > maxConfigurationVoters {
+		return model.Configuration{}, fmt.Errorf(
+			"configuration contains too many voters: %d",
+			voterCount,
+		)
+	}
+
+	voters := make([]model.NodeID, 0, voterCount)
+	seen := make(map[model.NodeID]struct{}, voterCount)
+
+	for i := uint32(0); i < voterCount; i++ {
+		var idLength uint32
+
+		if err := binary.Read(
+			reader,
+			binary.BigEndian,
+			&idLength,
+		); err != nil {
+			return model.Configuration{}, fmt.Errorf(
+				"decode voter %d ID length: %w",
+				i,
+				err,
+			)
+		}
+
+		if idLength == 0 {
+			return model.Configuration{}, fmt.Errorf(
+				"voter %d has empty ID",
+				i,
+			)
+		}
+
+		if idLength > maxConfigurationNodeID {
+			return model.Configuration{}, fmt.Errorf(
+				"voter %d ID is too long: %d",
+				i,
+				idLength,
+			)
+		}
+
+		if uint64(idLength) > uint64(reader.Len()) {
+			return model.Configuration{}, fmt.Errorf(
+				"voter %d ID is truncated",
+				i,
+			)
+		}
+
+		idBytes := make([]byte, idLength)
+
+		if _, err := reader.Read(idBytes); err != nil {
+			return model.Configuration{}, fmt.Errorf(
+				"decode voter %d ID: %w",
+				i,
+				err,
+			)
+		}
+
+		voterID := model.NodeID(string(idBytes))
+
+		if _, exists := seen[voterID]; exists {
+			return model.Configuration{}, fmt.Errorf(
+				"duplicate voter ID: %q",
+				voterID,
+			)
+		}
+
+		seen[voterID] = struct{}{}
+		voters = append(voters, voterID)
+	}
+
+	configuration := model.Configuration{
+		Voters: voters,
+	}
+
+	if err := validateConfiguration(configuration); err != nil {
+		return model.Configuration{}, err
+	}
+
+	return configuration, nil
+}
